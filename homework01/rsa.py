15c361e734d23effc024a3987008e171b0d3ab95
ZhukovaE <abnataliya@yandex.ru>
2025-11-19 16:43:13 +0300

    Исправлены ошибки в rsa

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -15,7 +15,7 @@ def is_prime(n: int) -> bool:
     # PUT YOUR CODE HERE
     if n < 2:
         return False
-    for i in range (2, n):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
